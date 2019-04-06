294e1b4f8c732071c9851bb16c5b2f982ae82b23
hyebeen_notebook <shb0328@gmail.com>
2019-04-06 23:49:42 +0900

    fab 버튼클릭시 카메라연동 createImageFile() 함수 작성 -아직사용은x

--- app/src/main/java/org/ssuss/myarcloset/MainActivity.java
+++ app/src/main/java/org/ssuss/myarcloset/MainActivity.java
@@ -1,16 +1,28 @@
 package org.ssuss.myarcloset;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
 
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,15 +35,27 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                */
-
-                
-
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
+
+    }
+
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyymmdd_HHmmss").format(new Date());
+        String imageFileName = "MyARCloset_"+timeStamp;
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDirectory
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
 }
