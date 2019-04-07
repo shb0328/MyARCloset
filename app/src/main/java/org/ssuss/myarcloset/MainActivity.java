35c15011c87734234297ffe9d4c0053f73133d5d
hyebeen_notebook <shb0328@gmail.com>
2019-04-08 06:15:41 +0900

    와 됐다. <갤러리에 저장>빼고 다됐다 ~!~!~!~! **takePhoto() 만들어 놓은 함수는 카메라앱을 실행시키는 거고, 사진이 찍히는 시점은 그후에 onActivityResult() 가 호출됬을 때였다...!!

--- app/src/main/java/org/ssuss/myarcloset/MainActivity.java
+++ app/src/main/java/org/ssuss/myarcloset/MainActivity.java
@@ -17,6 +17,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -42,10 +43,13 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
     static final int REQUEST_TAKE_PHOTO = 1;
     private static final int PERMISSIONS_REQUEST_CODE = 328;
     private static final String TAG = "Log :: ";
-    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.CAMERA, // 카메라
+
+    private String[] REQUIRED_PERMISSIONS  = {Manifest.permission.CAMERA, // 카메라
             Manifest.permission.WRITE_EXTERNAL_STORAGE};  // 외부 저장소
 
-    String uid;
+    private String uid;
+    private StorageReference storageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,13 +60,13 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
         uid = getIntent().getStringExtra("UID");
 
         /**
-         * firebase storage
+         * init firebase storage
          */
         // [START storage_field_initialization]
         FirebaseStorage storage = FirebaseStorage.getInstance();
 
         // Create a storage reference from our app
-        final StorageReference storageRef = storage.getReference();
+        storageRef = storage.getReference();
         /**
          * end
          */
@@ -110,26 +114,11 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
                 public void onClick(View view) {
                     int isOK = takePhoto();
                     if (isOK == SUCCESS) {
-                        Log.d(TAG,"**최근사진파일경로(addGallery전):"+currentPhotoPath);
-                        addImageToGallery();
-                        Log.d(TAG,"**최근사진파일경로(addGallery후):"+currentPhotoPath);
-                        
-                        //test
-                        Log.d(TAG,"**최근사진파일경로(bitmapFactory 전):"+currentPhotoPath);
-                        Bitmap bm = BitmapFactory.decodeFile(currentPhotoPath);
-                        Log.d(TAG,"**최근사진파일경로(bitmapFactory 후):"+currentPhotoPath);
-                        ImageView imageView = (ImageView)findViewById(R.id.imageView);
-                        imageView.setImageBitmap(bm);
-                        //test end
+//                        Log.d(TAG,"**최근사진파일경로(addGallery전):"+currentPhotoPath);
+//                        addImageToGallery();
+//                        Log.d(TAG,"**최근사진파일경로(addGallery후):"+currentPhotoPath);
 
-                        try{
-                            Log.d(TAG,"**최근사진파일경로(upload전):"+currentPhotoPath);
-                            uploadPhoto(storageRef,currentPhotoPath, uid);
-                            Toast.makeText(MainActivity.this, "사진을 firebase storage에 저장했습니다.", Toast.LENGTH_SHORT).show();
-                        }catch (FileNotFoundException fnfe){
-                            Log.d(TAG,"**사진찍었는데, 못찾겠대!");
-                            fnfe.getStackTrace();
-                        }
+
                     } else {
                         Toast.makeText(getApplicationContext(), "사진촬영에 실패했습니다.", Toast.LENGTH_SHORT).show();
                     }
@@ -140,12 +129,37 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
 
 
     }
-
     /**
      * end of onCreate
-     */
+     **/
 
-    private void uploadPhoto(StorageReference storageRef,String path,String userId) throws FileNotFoundException{
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+
+/** This is THE TIMING at which the picture was taken!! **/
+            //촬영한 사진 firebase storage에 저장
+            try{
+                uploadPhoto(currentPhotoPath, uid);
+                Toast.makeText(MainActivity.this, "사진을 firebase storage에 저장했습니다.", Toast.LENGTH_SHORT).show();
+            }catch (FileNotFoundException fnfe){
+                Log.d(TAG,"**FileNotFoundException**");
+                fnfe.getStackTrace();
+            }
+
+            //촬영한 사진 imageView에 띄우기
+            ImageView imageView = (ImageView)findViewById(R.id.imageView);
+            imageView.setImageURI(photoURI);
+
+            //TODO:촬영한 사진 갤러리에 저장
+            addImageToGallery();
+/****/
+        }
+    }
+
+    private void uploadPhoto(String path,String userId) throws FileNotFoundException{
         UploadTask uploadTask;
         String storagePath = "/user/"+userId+"/images/";
 
@@ -188,6 +202,57 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
          */
     }
 
+    Uri photoURI;
+    static final int SUCCESS = 1;
+    static final int FAIL = -1;
+    private int takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }catch(IOException ie){
+                ie.getStackTrace();
+            }
+            if(photoFile != null) {
+                photoURI = FileProvider.getUriForFile(
+                        this,
+                        "org.ssuss.myarcloset",
+                        photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
+
+                return SUCCESS;
+            }else {
+                return FAIL;
+            }
+        }else{
+            return FAIL;
+        }
+    }
+
+    String currentPhotoPath;
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "MyARCloset_"+timeStamp;
+        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDirectory
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void addImageToGallery() {
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri uri = Uri.fromFile(f);
+        intent.setData(uri);
+        this.sendBroadcast(intent);
+    }
+
     //requestPermissions callback method
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -223,57 +288,6 @@ public class MainActivity extends AppCompatActivity //implements ActivityCompat.
             // other 'case' lines to check for other
             // permissions this app might request
         }
-    }
-
-    Uri photoURI;
-    static final int SUCCESS = 1;
-    static final int FAIL = -1;
-    private int takePhoto() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null) {
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            }catch(IOException ie){
-                ie.getStackTrace();
-            }
-            if(photoFile != null) {
-                photoURI = FileProvider.getUriForFile(
-                        this,
-                        "org.ssuss.myarcloset",
-                        photoFile);
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(intent, REQUEST_TAKE_PHOTO);
-                return SUCCESS;
-            }else {
-                return FAIL;
-            }
-        }else{
-            return FAIL;
-        }
-    }
-
-    String currentPhotoPath;
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "MyARCloset_"+timeStamp;
-        File storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,
-                ".jpg",
-                storageDirectory
-        );
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
-    }
-
-    private void addImageToGallery() {
-        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri uri = Uri.fromFile(f);
-        intent.setData(uri);
-        this.sendBroadcast(intent);
-
     }
 
 }
