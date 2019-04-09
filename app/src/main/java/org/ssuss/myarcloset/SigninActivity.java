fd69ee5a8135960c8d7ced02eeb37cffc1e3d4af
hyebeen_notebook <shb0328@gmail.com>
2019-04-10 03:24:05 +0900

    로그인 후에 로그인 액티비티 없애도록 플래그 설정

--- app/src/main/java/org/ssuss/myarcloset/SigninActivity.java
+++ app/src/main/java/org/ssuss/myarcloset/SigninActivity.java
@@ -88,8 +88,8 @@ public class SigninActivity extends AppCompatActivity implements GoogleApiClient
                     //login
                     Intent intent = new Intent(SigninActivity.this,MainActivity.class);
                     intent.putExtra("UID",user.getUid());
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(intent);
-                    finish();
                 }else{
                     //logout
                 }
